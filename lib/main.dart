7e6489d2914eec70eafa034f22fe4306b0dca9b4
Abdallah Ghobary <abdualahghobary@gmail.com>
2026-01-29 18:33:17 +0200

    My App Green

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello,World!'), ),
+  runApp(const BirthDayApp());
+}
+
+class BirthDayApp extends StatelessWidget {
+  const BirthDayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add , color: Colors.white),
         ),
-        );
+        appBar: AppBar(backgroundColor: Colors.green, title: Text('My App')),
+        body: Center(
+          child: Text(
+            'Hello,World!',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
